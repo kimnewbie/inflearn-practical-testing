ae032a260e3dfd99134a2b5979250d11aa765b2f
youjinkim <gracecancho@gmail.com>
2025-01-04 12:48:02 +0900

    refactor: @DisplayName 설정

--- cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ cafekiosk/src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.unit;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
@@ -22,6 +23,8 @@ class CafeKioskTest {
         System.out.println(">>> 담긴 음료" + cafekiosk.getBeverages().get(0).getName());
     }
 
+    //    @DisplayName("음료 1개 추가 테스트")
+    @DisplayName("음료 1개를 추가하면 주문 목록에 담긴다.")
     @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
@@ -81,8 +84,11 @@ class CafeKioskTest {
         assertThat(cafeKiosk.getBeverages()).isEmpty();
     }
 
+
+    @DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
     @Test
     void calculateTotalPrice() {
+        // given
         CafeKiosk cafeKiosk = new CafeKiosk();
         Americano americano = new Americano();
         Latte latte = new Latte();
@@ -90,8 +96,10 @@ class CafeKioskTest {
         cafeKiosk.add(americano);
         cafeKiosk.add(latte);
 
+        // when
         int totalPrice = cafeKiosk.calculateTotalPrice();
 
+        // then
         assertThat(totalPrice).isEqualTo(8500);
     }
 
